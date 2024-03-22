0d6b17efde6afbf8699142b055f760df6c3c2e53
John Doe <opercik8@gmail.com>
2024-03-22 21:21:29 +0300

    new 4

--- 4.js
+++ 4.js
@@ -1,18 +1,14 @@
-function Intersection(arr1,arr2){
-    let result=[];
-    for(let i = 0; i<arr1.length;i++){
-        for(let j=0;j<arr2.length;j++){
-            if(arr1[i]===arr2[j]){
-                result.push(arr2[j]);
-            }
-        }
-    }
-    return result;
+function camelCases(arr1, arr2) {
+  let set1 = new Set(arr1);
+  let commonElements = arr2.filter(function (element) {
+    return set1.has(element);
+  });
+  return commonElements;
 }
 
-let arr1 = [1,2,3,4,5,6,7,8];
-let arr2 = [10,5,12,8,9,16];
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8];
+let arr2 = [10, 5, 12, 8, 9, 16];
 
-let intersection = Intersection(arr1,arr2);
+let intersection = camelCases(arr1, arr2);
 
 console.log(intersection);
